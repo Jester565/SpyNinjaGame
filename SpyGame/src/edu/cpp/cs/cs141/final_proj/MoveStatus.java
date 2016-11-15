fd6f63bb36112705b6cf5c3ce54513a442fb1629
Jester565 <ajcraig543@gmail.com>
2016-11-14 22:39:10 -0800

    Removed Item class.  Added documentation for MoveStatus.

--- SpyGame/src/edu/cpp/cs/cs141/final_proj/MoveStatus.java
+++ SpyGame/src/edu/cpp/cs/cs141/final_proj/MoveStatus.java
@@ -1,17 +1,35 @@
 package edu.cpp.cs.cs141.final_proj;
 
+/**
+ * Describes the result of a move in the {@link Grid}.
+ */
 public class MoveStatus {
 
+	/**
+	 * Describes the result of the attempted movement.
+	 */
 	public enum MOVE_RESULT {
 		ILLEGAL, LEGAL, WIN, LOSE, POWERUP
 	}
 	
+	/**
+	 * Creates instance of {@link MoveStatus} setting the parameters to the corresponding fields.
+	 * @param moveResult {@link #moveResult} is set to this value.
+	 * @param msg {@link #msg} is set to this value.
+	 */
 	public MoveStatus(MOVE_RESULT moveResult, String msg)
 	{
 		this.moveResult = moveResult;
 		this.msg = msg;
 	}
 	
+	/**
+	 * Describes the result of this attempted move.
+	 */
 	public MOVE_RESULT moveResult;
+	
+	/**
+	 * The message to be printed with a detailed description of the movement's result.
+	 */
 	public String msg;
 }
