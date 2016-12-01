146c073d06b97dacfaa6df87f5bd91bba4c3028b
Jester565 <ajcraig543@gmail.com>
2016-11-30 19:26:53 -0800

    Revert gun max ammo to 1 bullet

--- SpyGame/src/edu/cpp/cs/cs141/final_proj/Gun.java
+++ SpyGame/src/edu/cpp/cs/cs141/final_proj/Gun.java
@@ -25,7 +25,7 @@ public class Gun extends Weapon implements Serializable {
 	/**
 	 * This integer stores the maximum number of rounds that the {@link Spy} can start with.
 	 */
-	public static final int MAX_ROUNDS = 2;
+	public static final int MAX_ROUNDS = 1;
 	
 	/**
 	 * this integer stores the amount of grid spaces a {@link Character}'s weapon can travel. Guns
@@ -50,7 +50,7 @@ public class Gun extends Weapon implements Serializable {
 	 */
 	public void addBullet(int numRoundsAdded){
 		if(this.numRounds < MAX_ROUNDS){
-		this.numRounds += numRoundsAdded;
+			this.numRounds += numRoundsAdded;
 		}
 	}
 	
